d1c2f4236ba65c47f68fc35646976807839062d8
liu shiwei <liushiwei@gmail.com>
2022-02-08 23:30:03 +0800

    编译错误

--- arch/mips/loongson1/common/prom.c
+++ arch/mips/loongson1/common/prom.c
@@ -142,6 +142,7 @@ void __init prom_init(void)
 	/* 只用于ls1b的lcd接口传vga接口时使用，如云终端，
 	 因为使用vga时，pll通过计算难以得到合适的分频;给LS1X_CLK_PLL_FREQ和LS1X_CLK_PLL_DIV
 	 寄存器一个固定的值 */
+#include "video_modes.c"
 	{
 	int i;
 	int default_xres = 800;
